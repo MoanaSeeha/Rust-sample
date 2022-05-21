36b136ecbfab73f8feb8abc5f1c4e7cd110fbf02
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-21 22:06:28 +0800

    gggg

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-80b5d0f509114918.d
@@ -0,0 +1,5 @@
+f:\Gitworks\Rust-sample\hello_cargo\target\rls\debug\deps\hello_cargo-80b5d0f509114918.rmeta: src\main.rs
+
+f:\Gitworks\Rust-sample\hello_cargo\target\rls\debug\deps\hello_cargo-80b5d0f509114918.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-c5ee3f8973577d48.d
@@ -0,0 +1,5 @@
+f:\Gitworks\Rust-sample\hello_cargo\target\rls\debug\deps\hello_cargo-c5ee3f8973577d48.rmeta: src\main.rs
+
+f:\Gitworks\Rust-sample\hello_cargo\target\rls\debug\deps\hello_cargo-c5ee3f8973577d48.d: src\main.rs
+
+src\main.rs:
